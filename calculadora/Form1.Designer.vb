1ab0b2d8d6ffbd73b2404ef55fc25fbfc899b28b
fmelods <felipemelodesousa4@gmail.com>
2024-07-24 23:23:11 -0300

    Adicionado calculado em VB.NET com evento de KeyDown

---
+++ calculadora/Form1.Designer.vb
@@ -0,0 +1,266 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        tbnLimpar = New Button()
+        tbnIgualdade = New Button()
+        tbnVirgula = New Button()
+        tbn9 = New Button()
+        tbn6 = New Button()
+        tbn3 = New Button()
+        tbn0 = New Button()
+        tbn8 = New Button()
+        tbn5 = New Button()
+        tbn2 = New Button()
+        tbnPonto = New Button()
+        tbn7 = New Button()
+        tbn4 = New Button()
+        tbn1 = New Button()
+        tbnDividir = New Button()
+        tbnMultiplicar = New Button()
+        tbnSubtrair = New Button()
+        tbnSomar = New Button()
+        txtDisplay = New TextBox()
+        SuspendLayout()
+        ' 
+        ' tbnLimpar
+        ' 
+        tbnLimpar.Location = New Point(218, 106)
+        tbnLimpar.Name = "tbnLimpar"
+        tbnLimpar.Size = New Size(44, 36)
+        tbnLimpar.TabIndex = 37
+        tbnLimpar.Text = "C"
+        tbnLimpar.UseVisualStyleBackColor = True
+        ' 
+        ' tbnIgualdade
+        ' 
+        tbnIgualdade.Location = New Point(218, 159)
+        tbnIgualdade.Name = "tbnIgualdade"
+        tbnIgualdade.Size = New Size(45, 115)
+        tbnIgualdade.TabIndex = 36
+        tbnIgualdade.Text = "="
+        tbnIgualdade.UseVisualStyleBackColor = True
+        ' 
+        ' tbnVirgula
+        ' 
+        tbnVirgula.Location = New Point(158, 251)
+        tbnVirgula.Name = "tbnVirgula"
+        tbnVirgula.Size = New Size(45, 23)
+        tbnVirgula.TabIndex = 35
+        tbnVirgula.Text = ","
+        tbnVirgula.UseVisualStyleBackColor = True
+        ' 
+        ' tbn9
+        ' 
+        tbn9.Location = New Point(158, 206)
+        tbn9.Name = "tbn9"
+        tbn9.Size = New Size(45, 23)
+        tbn9.TabIndex = 34
+        tbn9.Text = "9"
+        tbn9.UseVisualStyleBackColor = True
+        ' 
+        ' tbn6
+        ' 
+        tbn6.Location = New Point(158, 159)
+        tbn6.Name = "tbn6"
+        tbn6.Size = New Size(45, 23)
+        tbn6.TabIndex = 33
+        tbn6.Text = "6"
+        tbn6.UseVisualStyleBackColor = True
+        ' 
+        ' tbn3
+        ' 
+        tbn3.Location = New Point(158, 113)
+        tbn3.Name = "tbn3"
+        tbn3.Size = New Size(45, 23)
+        tbn3.TabIndex = 32
+        tbn3.Text = "3"
+        tbn3.UseVisualStyleBackColor = True
+        ' 
+        ' tbn0
+        ' 
+        tbn0.Location = New Point(91, 251)
+        tbn0.Name = "tbn0"
+        tbn0.Size = New Size(45, 23)
+        tbn0.TabIndex = 31
+        tbn0.Text = "0"
+        tbn0.UseVisualStyleBackColor = True
+        ' 
+        ' tbn8
+        ' 
+        tbn8.Location = New Point(91, 206)
+        tbn8.Name = "tbn8"
+        tbn8.Size = New Size(45, 23)
+        tbn8.TabIndex = 30
+        tbn8.Text = "8"
+        tbn8.UseVisualStyleBackColor = True
+        ' 
+        ' tbn5
+        ' 
+        tbn5.Location = New Point(92, 159)
+        tbn5.Name = "tbn5"
+        tbn5.Size = New Size(45, 23)
+        tbn5.TabIndex = 29
+        tbn5.Text = "5"
+        tbn5.UseVisualStyleBackColor = True
+        ' 
+        ' tbn2
+        ' 
+        tbn2.Location = New Point(92, 113)
+        tbn2.Name = "tbn2"
+        tbn2.Size = New Size(45, 23)
+        tbn2.TabIndex = 28
+        tbn2.Text = "2"
+        tbn2.UseVisualStyleBackColor = True
+        ' 
+        ' tbnPonto
+        ' 
+        tbnPonto.Location = New Point(26, 251)
+        tbnPonto.Name = "tbnPonto"
+        tbnPonto.Size = New Size(45, 23)
+        tbnPonto.TabIndex = 27
+        tbnPonto.Text = "."
+        tbnPonto.UseVisualStyleBackColor = True
+        ' 
+        ' tbn7
+        ' 
+        tbn7.Location = New Point(26, 206)
+        tbn7.Name = "tbn7"
+        tbn7.Size = New Size(45, 23)
+        tbn7.TabIndex = 26
+        tbn7.Text = "7"
+        tbn7.UseVisualStyleBackColor = True
+        ' 
+        ' tbn4
+        ' 
+        tbn4.Location = New Point(26, 159)
+        tbn4.Name = "tbn4"
+        tbn4.Size = New Size(45, 23)
+        tbn4.TabIndex = 25
+        tbn4.Text = "4"
+        tbn4.UseVisualStyleBackColor = True
+        ' 
+        ' tbn1
+        ' 
+        tbn1.Location = New Point(26, 113)
+        tbn1.Name = "tbn1"
+        tbn1.Size = New Size(45, 23)
+        tbn1.TabIndex = 24
+        tbn1.Text = "1"
+        tbn1.UseVisualStyleBackColor = True
+        ' 
+        ' tbnDividir
+        ' 
+        tbnDividir.Location = New Point(219, 71)
+        tbnDividir.Name = "tbnDividir"
+        tbnDividir.Size = New Size(44, 23)
+        tbnDividir.TabIndex = 23
+        tbnDividir.Text = "/"
+        tbnDividir.UseVisualStyleBackColor = True
+        ' 
+        ' tbnMultiplicar
+        ' 
+        tbnMultiplicar.Location = New Point(158, 71)
+        tbnMultiplicar.Name = "tbnMultiplicar"
+        tbnMultiplicar.Size = New Size(46, 23)
+        tbnMultiplicar.TabIndex = 22
+        tbnMultiplicar.Text = "X"
+        tbnMultiplicar.UseVisualStyleBackColor = True
+        ' 
+        ' tbnSubtrair
+        ' 
+        tbnSubtrair.Location = New Point(91, 71)
+        tbnSubtrair.Name = "tbnSubtrair"
+        tbnSubtrair.Size = New Size(46, 23)
+        tbnSubtrair.TabIndex = 21
+        tbnSubtrair.Text = "-"
+        tbnSubtrair.UseVisualStyleBackColor = True
+        ' 
+        ' tbnSomar
+        ' 
+        tbnSomar.Location = New Point(26, 71)
+        tbnSomar.Name = "tbnSomar"
+        tbnSomar.Size = New Size(45, 23)
+        tbnSomar.TabIndex = 20
+        tbnSomar.Text = "+"
+        tbnSomar.UseVisualStyleBackColor = True
+        ' 
+        ' txtDisplay
+        ' 
+        txtDisplay.Font = New Font("Segoe UI", 14F)
+        txtDisplay.Location = New Point(26, 24)
+        txtDisplay.Name = "txtDisplay"
+        txtDisplay.Size = New Size(237, 32)
+        txtDisplay.TabIndex = 19
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(289, 298)
+        Controls.Add(tbnLimpar)
+        Controls.Add(tbnIgualdade)
+        Controls.Add(tbnVirgula)
+        Controls.Add(tbn9)
+        Controls.Add(tbn6)
+        Controls.Add(tbn3)
+        Controls.Add(tbn0)
+        Controls.Add(tbn8)
+        Controls.Add(tbn5)
+        Controls.Add(tbn2)
+        Controls.Add(tbnPonto)
+        Controls.Add(tbn7)
+        Controls.Add(tbn4)
+        Controls.Add(tbn1)
+        Controls.Add(tbnDividir)
+        Controls.Add(tbnMultiplicar)
+        Controls.Add(tbnSubtrair)
+        Controls.Add(tbnSomar)
+        Controls.Add(txtDisplay)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents tbnLimpar As Button
+    Friend WithEvents tbnIgualdade As Button
+    Friend WithEvents tbnVirgula As Button
+    Friend WithEvents tbn9 As Button
+    Friend WithEvents tbn6 As Button
+    Friend WithEvents tbn3 As Button
+    Friend WithEvents tbn0 As Button
+    Friend WithEvents tbn8 As Button
+    Friend WithEvents tbn5 As Button
+    Friend WithEvents tbn2 As Button
+    Friend WithEvents tbnPonto As Button
+    Friend WithEvents tbn7 As Button
+    Friend WithEvents tbn4 As Button
+    Friend WithEvents tbn1 As Button
+    Friend WithEvents tbnDividir As Button
+    Friend WithEvents tbnMultiplicar As Button
+    Friend WithEvents tbnSubtrair As Button
+    Friend WithEvents tbnSomar As Button
+    Friend WithEvents txtDisplay As TextBox
+
+End Class
---
+++ calculadora/Form1.vb
@@ -0,0 +1,139 @@
+﻿Public Class Form1
+    Dim vPrimeiro As Double
+    Dim vSinal As String
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.KeyPreview = True
+    End Sub
+
+    Private Sub tbn1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbn1.Click
+        txtDisplay.Text &= "1"
+    End Sub
+
+    Private Sub tbn2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbn2.Click
+        txtDisplay.Text &= "2"
+    End Sub
+
+    Private Sub tbn3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbn3.Click
+        txtDisplay.Text &= "3"
+    End Sub
+
+    Private Sub tbn4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbn4.Click
+        txtDisplay.Text &= "4"
+    End Sub
+
+    Private Sub tbn5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbn5.Click
+        txtDisplay.Text &= "5"
+    End Sub
+
+    Private Sub tbn6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbn6.Click
+        txtDisplay.Text &= "6"
+    End Sub
+
+    Private Sub tbn7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbn7.Click
+        txtDisplay.Text &= "7"
+    End Sub
+
+    Private Sub tbn8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbn8.Click
+        txtDisplay.Text &= "8"
+    End Sub
+
+    Private Sub tbn9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbn9.Click
+        txtDisplay.Text &= "9"
+    End Sub
+
+    Private Sub tbnPonto_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbnPonto.Click
+        If Not txtDisplay.Text.Contains(".") Then
+            txtDisplay.Text &= "."
+        End If
+    End Sub
+
+    Private Sub tbn0_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbn0.Click
+        txtDisplay.Text &= "0"
+    End Sub
+
+    Private Sub tbnVirgula_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbnVirgula.Click
+        If Not txtDisplay.Text.Contains(",") Then
+            txtDisplay.Text &= ","
+        End If
+    End Sub
+
+    Private Sub tbnSomar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbnSomar.Click
+        If Double.TryParse(txtDisplay.Text, vPrimeiro) Then
+            txtDisplay.Text = ""
+            vSinal = "+"
+        End If
+    End Sub
+
+    Private Sub tbnSubstrair_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbnSubtrair.Click
+        If Double.TryParse(txtDisplay.Text, vPrimeiro) Then
+            txtDisplay.Text = ""
+            vSinal = "-"
+        End If
+    End Sub
+
+    Private Sub tbnMultiplicar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbnMultiplicar.Click
+        If Double.TryParse(txtDisplay.Text, vPrimeiro) Then
+            txtDisplay.Text = ""
+            vSinal = "X"
+        End If
+    End Sub
+
+    Private Sub tbnDividir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbnDividir.Click
+        If Double.TryParse(txtDisplay.Text, vPrimeiro) Then
+            txtDisplay.Text = ""
+            vSinal = "/"
+        End If
+    End Sub
+
+    Private Sub tbnIgualdade_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbnIgualdade.Click
+        Dim vSegundo As Double
+        If Double.TryParse(txtDisplay.Text, vSegundo) Then
+            Select Case vSinal
+                Case "+"
+                    txtDisplay.Text = (vPrimeiro + vSegundo).ToString()
+                Case "-"
+                    txtDisplay.Text = (vPrimeiro - vSegundo).ToString()
+                Case "X"
+                    txtDisplay.Text = (vPrimeiro * vSegundo).ToString()
+                Case "/"
+                    If vSegundo <> 0 Then
+                        txtDisplay.Text = (vPrimeiro / vSegundo).ToString()
+                    End If
+            End Select
+        End If
+    End Sub
+
+    Private Sub tbnLimpar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbnLimpar.Click
+        txtDisplay.Text = ""
+        vSinal = ""
+        vPrimeiro = 0
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        Select Case e.KeyCode
+            Case Keys.Add
+                tbnSomar.PerformClick()
+            Case Keys.Subtract
+                tbnSubtrair.PerformClick()
+            Case Keys.Multiply
+                tbnMultiplicar.PerformClick()
+            Case Keys.Divide
+                tbnDividir.PerformClick()
+            Case Keys.Enter
+                e.Handled = True
+                e.SuppressKeyPress = True
+                tbnIgualdade.PerformClick()
+            Case Keys.Escape
+                tbnLimpar.PerformClick()
+        End Select
+    End Sub
+
+    Private Sub txtDisplay_KeyPress(ByVal sender As Object, ByVal e As KeyPressEventArgs) Handles txtDisplay.KeyPress
+        Dim charsPermitidos As String = "0123456789.,+-*/"
+
+        If Not charsPermitidos.Contains(e.KeyChar) AndAlso e.KeyChar <> ChrW(Keys.Back) Then
+            e.Handled = True
+        End If
+    End Sub
+End Class
